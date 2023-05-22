7c7484c34d725dd5a9351afffb34f421d8e6f99b
Uladzislau_Melikhau <vlad_melichov2014@mail.ru>
2023-05-22 18:15:30 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from modules import functions
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print(f"It is {now}")
+
+while True:
+    user_action = input("type add, show, edit, remove or exit:")
+    user_action = user_action.strip()
+
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+
+        todos = functions.get_todos()
+
+        todos.append(todo + "\n")
+
+        functions.write_todos(todos)
+
+    elif user_action.startswith("show"):
+        todos = functions.get_todos()
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            print(f"{index+1}-{item}")
+
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
+
+            todos = functions.get_todos()
+
+            new_todo = input("Enter a new todo:")
+            todos[number] = new_todo + "\n"
+
+            functions.write_todos(todos)
+        except ValueError:
+            print("Your command is not valid")
+            continue
+
+    elif user_action.startswith("remove"):
+        try:
+            number = int(user_action[7:])
+
+            todos = functions.get_todos()
+
+            index = number-1
+            todo_to_remove = todos[index].strip("\n")
+            todos.pop(index)
+
+            functions.write_todos(todos)
+
+            print(f"Todo \"{todo_to_remove}\" was removed from the list")
+        except ValueError:
+            print("There is no item with that number")
+            continue
+        except IndexError:
+            print("There is no item with that number")
+            continue
+
+    elif 'exit' in user_action:
+        break
+    else:
+        print("Command is not valid!")
+
+print("Bye!")

072cd7ce0f470ea1fddc0a0a855cca9f40d9fea5
Uladzislau_Melikhau <vlad_melichov2014@mail.ru>
2023-06-01 20:35:47 +0300

    Update Add button

--- GUI.py
+++ GUI.py
@@ -8,7 +8,8 @@ sg.theme("DarkTeal7")
 clock = sg.Text('', key="clock")
 label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter a todo", key = "todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(image_size=(35,35), image_source="add.png", mouseover_colors="LightBlue2",
+                       tooltip="Add todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
